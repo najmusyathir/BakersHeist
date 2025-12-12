958fe7bdd6e740d3efaba270dd304dc02252fb91
Najmu Syathir <134459622+najmusyathir@users.noreply.github.com>
2025-12-12 12:29:23 +0800

    Modify: Remove music autoplay

--- scripts/music.js
+++ scripts/music.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Play the background music when the DOM is fully loaded
-    var audio = document.getElementById("backgroundMusic");
-    audio.play();
-    audio.volume=0.2;
+    // // Play the background music when the DOM is fully loaded
+    // var audio = document.getElementById("backgroundMusic");
+    // audio.play();
+    // audio.volume=0.2;
 
-    // Save the current time of the audio when navigating to a new page
-    document.querySelectorAll("a").forEach(function (link) {
-        link.addEventListener("click", function () {
-            localStorage.setItem("audioTime", audio.currentTime);
-        });
-    });
+    // // Save the current time of the audio when navigating to a new page
+    // document.querySelectorAll("a").forEach(function (link) {
+    //     link.addEventListener("click", function () {
+    //         localStorage.setItem("audioTime", audio.currentTime);
+    //     });
+    // });
 
-    // Restore the saved time and play the audio when the new page is loaded
-    var savedTime = localStorage.getItem("audioTime");
-    if (savedTime) {
-        audio.currentTime = savedTime;
-    }
+    // // Restore the saved time and play the audio when the new page is loaded
+    // var savedTime = localStorage.getItem("audioTime");
+    // if (savedTime) {
+    //     audio.currentTime = savedTime;
+    // }
 });
